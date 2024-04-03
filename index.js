b0a79e7ed1abc9cfca0a84393e5e23f7fe90af33
Sabrun <officialsabrun@gmail.com>
2024-04-03 19:30:49 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
 
 mongoose
   .connect(
@@ -8,3 +11,13 @@ mongoose
   )
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.log("Could not connect to MongoDB...", err));
+
+const players = require("./data/players");
+const teams = require("./data/teams");
+const conferences = require("./data/conference");
+
+app.use(express.static("public"));
+app.use(express.static("css"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+app.use(logger);
